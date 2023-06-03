d9f156217ea0c60fa0290d894f82a3cf6e981b56
Dricera <7955817+Dricera@users.noreply.github.com>
2023-06-03 23:59:58 +0530

    scaffolding for pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Test from './components/Test'
 import { RecoilRoot } from 'recoil'
+import { RecoilEnv } from 'recoil'
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false
+
 function App() {
+  const PlaceHolder = () => {
+    return(
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>Vite + React</h1>
+    </div>
+    )}
   return (
     <RecoilRoot>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <PlaceHolder />
       <Test />
     </RecoilRoot>
   )
